e96a84ead4153ea073e8284e41fcda5880ffb836
Ahsan11O <143723524+Ahsan11O@users.noreply.github.com>
2026-07-27 12:32:41 +0500

    Refactor script.js to add hamburger menu functionality

--- script.js
+++ script.js
@@ -448,3 +448,24 @@ if (contactForm) {
     });
 
 }
+// =========================
+// HAMBURGER MENU
+// =========================
+
+const menuToggle = document.getElementById("menuToggle");
+const navbar = document.getElementById("navbar");
+
+if(menuToggle && navbar){
+
+    menuToggle.addEventListener("click",()=>{
+
+        navbar.classList.toggle("active");
+
+        menuToggle.innerHTML =
+        navbar.classList.contains("active")
+        ? "✖"
+        : "☰";
+
+    });
+
+}
